17b9ca775278610007be68310838535763442a00
genyarko <115112347+genyarko@users.noreply.github.com>
2023-09-25 22:54:16 +0000

    Create 100-createIteratorObject.js

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,14 @@
+export default function createIteratorObject(report) {
+  const iterator = {
+    [Symbol.iterator]: function* () {
+      for (const department in report.allEmployees) {
+        const employees = report.allEmployees[department];
+        for (const employee of employees) {
+          yield employee;
+        }
+      }
+    },
+  };
+
+  return iterator;
+}
