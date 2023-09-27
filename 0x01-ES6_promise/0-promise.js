185187e982def48b26322409532d593e80320c0c
genyarko <115112347+genyarko@users.noreply.github.com>
2023-09-27 12:39:19 +0000

    Create 0-promise.js

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,7 @@
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // You can perform asynchronous operations here
+    // For this example, we'll resolve immediately
+    resolve('API response data'); // Replace with your API call
+  });
+}
