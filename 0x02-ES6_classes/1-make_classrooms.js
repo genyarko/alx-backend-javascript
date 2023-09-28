abf5ac9bcf29a426574a4f129e6320aa563b2bfd
genyarko <115112347+genyarko@users.noreply.github.com>
2023-09-28 16:56:56 +0000

    Create 1-make_classrooms.js

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,9 @@
+import ClassRoom from './0-classroom.js';
+
+function initializeRooms() {
+  const sizes = [19, 20, 34];
+  const rooms = sizes.map((size) => new ClassRoom(size));
+  return rooms;
+}
+
+export default initializeRooms;
