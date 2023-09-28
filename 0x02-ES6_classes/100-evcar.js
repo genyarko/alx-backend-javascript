8649de3f7d11f9101491055d85009ed1821466f0
genyarko <115112347+genyarko@users.noreply.github.com>
2023-09-28 17:54:02 +0000

    Update 100-evcar.js

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,6 +1,6 @@
-import Car from './10-car.js';
+import Car from './10-car';
 
-class EVCar extends Car {
+export default class EVCar extends Car  {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
 
@@ -11,9 +11,11 @@ class EVCar extends Car {
     return this._range;
   }
 
+   set range(value) {
+    this._range = value;
+  }
+
   cloneCar() {
     return new Car(this.brand, this.motor, this.color);
   }
 }
-
-export default EVCar;
