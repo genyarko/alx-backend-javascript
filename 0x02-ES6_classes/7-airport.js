55ad664bdf8570fd3c954ee926ccd6486ad3f27f
genyarko <115112347+genyarko@users.noreply.github.com>
2023-09-28 17:33:08 +0000

    Create 7-airport.js

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,21 @@
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  // Getter for name
+  get name() {
+    return this._name;
+  }
+
+  // Getter for code
+  get code() {
+    return this._code;
+  }
+
+  // Default string description of the class
+  toString() {
+    return this._code;
+  }
+}
