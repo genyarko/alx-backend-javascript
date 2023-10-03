2717a29ba210a0efc5fa91834d5902da1bbad9d6
genyarko <115112347+genyarko@users.noreply.github.com>
2023-10-03 16:50:56 +0000

    Update 1-get_list_student_ids.js

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,3 +1,12 @@
+/**
+ * Retrieves ids from a list of students.
+ * @param {{
+ *   id: Number,
+ *   firstName: String,
+ *   location: String
+ * }[]} students - The list of students.
+ * @returns
+ */
 export default function getListStudentIds(students) {
   if (!Array.isArray(students)) {
     return [];
