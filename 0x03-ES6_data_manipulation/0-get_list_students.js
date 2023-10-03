79e066f48aad35f38e63ce4631530f3e8a8b69f7
genyarko <115112347+genyarko@users.noreply.github.com>
2023-10-03 16:47:09 +0000

    Update 0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,23 +1,7 @@
-function getListStudents() {
-  // Create an array of objects
-  const students = [
-    {
-      id: 1,
-      firstName: "Guillaume",
-      location: "San Francisco",
-    },
-    {
-      id: 2,
-      firstName: "James",
-      location: "Columbia",
-    },
-    {
-      id: 5,
-      firstName: "Serena",
-      location: "San Francisco",
-    },
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }
   ];
-
-  // Return the array of objects
-  return students;
 }
