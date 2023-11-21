cdfda6c06a6fd52dcefa818c2d11d09f8ef04823
genyarko <115112347+genyarko@users.noreply.github.com>
2023-11-21 09:48:40 +0000

    Update 4-http.js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,24 +1,50 @@
 const http = require('http');
+const url = require('url');
+const { countStudents } = require('./3-read_file_async');
 
-const PORT = 1245;
-const HOST = 'localhost';
-const app = http.createServer();
+// Create an HTTP server
+const app = http.createServer((req, res) => {
+  // Parse the URL to extract the path
+  const { pathname } = url.parse(req.url, true);
 
-app.on('request', (_, res) => {
-  const responseText = 'Hello Holberton School!';
-
+  // Set the content type to plain text
   res.setHeader('Content-Type', 'text/plain');
-  res.setHeader('Content-Length', responseText.length);
-  res.statusCode = 200;
-  res.end(responseText);
+
+  // Handle different URL paths
+  if (pathname === '/') {
+    // Respond with "Hello Holberton School!" for the root path
+    res.end('Hello Holberton School!\n');
+  } else if (pathname === '/students') {
+    // Respond with student information for the /students path
+    // The name of the database file is passed as a command line argument
+    const databaseFileName = process.argv[2];
+    if (!databaseFileName) {
+      res.end('Error: Database file name is missing.\n');
+      return;
+    }
+
+    // Call the countStudents function from 3-read_file_async.js
+    countStudents(databaseFileName)
+      .then((studentInfo) => {
+        // Display the student information
+        res.end(`This is the list of our students\n${studentInfo}`);
+      })
+      .catch((error) => {
+        // Handle errors
+        res.end(`Error: ${error.message}\n`);
+      });
+  } else {
+    // Respond with a 404 error for unknown paths
+    res.statusCode = 404;
+    res.end('Error 404: Not Found\n');
+  }
 });
 
-app.on('error', (error) => {
-  console.error(`Server error: ${error.message}`);
+// Listen on port 1245
+const PORT = 1245;
+app.listen(PORT, () => {
+  console.log(`Server is running and listening on port ${PORT}`);
 });
 
-app.listen(PORT, HOST, () => {
-  process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
-});
-
+// Export the app variable
 module.exports = app;
