1d90021b120307dca1b0f16af9d5c081be66691f
genyarko <115112347+genyarko@users.noreply.github.com>
2023-11-21 09:37:07 +0000

    Update 2-read_file.js

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,42 +1,55 @@
 const fs = require('fs');
-
-/**
- * Counts the students in a CSV data file.
- * @param {String} dataPath The path to the CSV data file.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
- */
 const countStudents = (dataPath) => {
+  // Check if the file exists
   if (!fs.existsSync(dataPath)) {
-    throw new Error('Cannot load the database');
+    throw new Error('Cannot load the database: File does not exist.');
   }
-  if (!fs.statSync(dataPath).isFile()) {
-    throw new Error('Cannot load the database');
-  }
-  const fileLines = fs
-    .readFileSync(dataPath, 'utf-8')
-    .toString('utf-8')
-    .trim()
-    .split('\n');
-  const studentGroups = {};
-  const dbFieldNames = fileLines[0].split(',');
-  const studentPropNames = dbFieldNames.slice(0, dbFieldNames.length - 1);
 
+  // Check if the path points to a valid file
+  if (!fs.statSync(dataPath).isFile()) {
+    throw new Error('Cannot load the database: Invalid file path.');
+  }
+
+  // Read the file content synchronously
+  const fileContent = fs.readFileSync(dataPath, 'utf-8').trim();
+
+  // Split the file content into lines
+  const fileLines = fileContent.split('\n');
+
+  // Initialize an object to store student records grouped by field
+  const studentGroups = {};
+
+  // Extract column names from the first line
+  const dbFieldNames = fileLines[0].split(',');
+  // Exclude the last column (field) from student properties
+  const studentPropNames = dbFieldNames.slice(0, -1);
+
+  // Iterate over each line (skipping the header) to process student records
   for (const line of fileLines.slice(1)) {
     const studentRecord = line.split(',');
-    const studentPropValues = studentRecord.slice(0, studentRecord.length - 1);
+    // Exclude the last column (field) from student properties
+    const studentPropValues = studentRecord.slice(0, -1);
     const field = studentRecord[studentRecord.length - 1];
-    if (!Object.keys(studentGroups).includes(field)) {
+
+    // Initialize an array for the field if it doesn't exist in the groups object
+    if (!studentGroups[field]) {
       studentGroups[field] = [];
     }
-    const studentEntries = studentPropNames
-      .map((propName, idx) => [propName, studentPropValues[idx]]);
+
+    // Create an array of entries (property name and value) for each student
+    const studentEntries = studentPropNames.map((propName, idx) => [propName, studentPropValues[idx]]);
+
+    // Convert the array of entries to an object and add it to the corresponding field group
     studentGroups[field].push(Object.fromEntries(studentEntries));
   }
 
-  const totalStudents = Object
-    .values(studentGroups)
-    .reduce((pre, cur) => (pre || []).length + cur.length);
+  // Calculate the total number of students across all fields
+  const totalStudents = Object.values(studentGroups).reduce((acc, group) => acc + group.length, 0);
+
+  // Display the total number of students
   console.log(`Number of students: ${totalStudents}`);
+
+  // Display the number of students in each field along with their names
   for (const [field, group] of Object.entries(studentGroups)) {
     const studentNames = group.map((student) => student.firstname).join(', ');
     console.log(`Number of students in ${field}: ${group.length}. List: ${studentNames}`);
@@ -44,3 +57,4 @@ const countStudents = (dataPath) => {
 };
 
 module.exports = countStudents;
+
