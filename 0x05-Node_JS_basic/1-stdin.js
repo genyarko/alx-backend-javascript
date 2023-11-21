6165931741526ae2c419c12a482fa7be54de8749
genyarko <115112347+genyarko@users.noreply.github.com>
2023-11-21 09:26:09 +0000

    Update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -11,14 +11,21 @@ console.log('Welcome to Holberton School, what is your name?');
 
 rl.question('', (name) => {
   console.log(`Your name is: ${name}`);
-  console.log('Press Ctrl+C to exit or enter another name:');
+  console.log('Type "exit" to close the program.');
 });
 
+// Listen for the "line" event to detect user input
+rl.on('line', (input) => {
+  if (input.toLowerCase() === 'exit') {
+    console.log('This important software is now closing');
+    rl.close();
+  } else {
+    // Handle other input if needed
+    console.log(`You typed: ${input}`);
+  }
+});
+
+// Listen for the "close" event to perform cleanup
 rl.on('close', () => {
-  console.log('This important software is now closing');
-});
-
-// Listen for the 'SIGINT' event (Ctrl+C) to close the program gracefully
-process.on('SIGINT', () => {
-  rl.close();
+  process.exit(0);
 });
