a2c3d77abf5ae2caef996dccabfdd91d31c02ec4
genyarko <115112347+genyarko@users.noreply.github.com>
2023-11-21 11:30:32 +0000

    Update StudentsController.js

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,33 +1,25 @@
 import readDatabase from '../utils';
 
-/**
- * The list of supported majors.
- */
+// Array of valid major values
 const VALID_MAJORS = ['CS', 'SWE'];
 
-/**
- * Contains the student-related route handlers.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
- */
+// Class definition for the StudentsController
 class StudentsController {
+  // Static method to get information for all students
   static getAllStudents(request, response) {
+    // Get the path to the database file from command line arguments
     const dataPath = process.argv.length > 2 ? process.argv[2] : '';
 
+    // Read the database asynchronously
     readDatabase(dataPath)
       .then((studentGroups) => {
+        // Prepare the response parts for displaying student information
         const responseParts = ['This is the list of our students'];
-        // A comparison function for ordering a list of strings in ascending
-        // order by alphabetic order and case insensitive
-        const cmpFxn = (a, b) => {
-          if (a[0].toLowerCase() < b[0].toLowerCase()) {
-            return -1;
-          }
-          if (a[0].toLowerCase() > b[0].toLowerCase()) {
-            return 1;
-          }
-          return 0;
-        };
+        
+        // Comparator function for sorting student groups by field name
+        const cmpFxn = (a, b) => a[0].toLowerCase().localeCompare(b[0].toLowerCase());
 
+        // Loop through the sorted student groups and build the response
         for (const [field, group] of Object.entries(studentGroups).sort(cmpFxn)) {
           responseParts.push([
             `Number of students in ${field}: ${group.length}.`,
@@ -35,40 +27,52 @@ class StudentsController {
             group.map((student) => student.firstname).join(', '),
           ].join(' '));
         }
+        
+        // Send a 200 status response with the joined response parts
         response.status(200).send(responseParts.join('\n'));
       })
       .catch((err) => {
-        response
-          .status(500)
-          .send(err instanceof Error ? err.message : err.toString());
+        // Send a 500 status response with the error message if an error occurs
+        response.status(500).send(err instanceof Error ? err.message : err.toString());
       });
   }
 
+  // Static method to get information for students in a specific major
   static getAllStudentsByMajor(request, response) {
+    // Get the path to the database file from command line arguments
     const dataPath = process.argv.length > 2 ? process.argv[2] : '';
+    
+    // Extract the major parameter from the request
     const { major } = request.params;
 
+    // Check if the major parameter is valid
     if (!VALID_MAJORS.includes(major)) {
       response.status(500).send('Major parameter must be CS or SWE');
       return;
     }
+
+    // Read the database asynchronously
     readDatabase(dataPath)
       .then((studentGroups) => {
+        // Initialize the response text
         let responseText = '';
 
+        // Check if the specified major exists in the database
         if (Object.keys(studentGroups).includes(major)) {
           const group = studentGroups[major];
           responseText = `List: ${group.map((student) => student.firstname).join(', ')}`;
         }
+
+        // Send a 200 status response with the response text
         response.status(200).send(responseText);
       })
       .catch((err) => {
-        response
-          .status(500)
-          .send(err instanceof Error ? err.message : err.toString());
+        // Send a 500 status response with the error message if an error occurs
+        response.status(500).send(err instanceof Error ? err.message : err.toString());
       });
   }
 }
 
+// Export the StudentsController class
 export default StudentsController;
 module.exports = StudentsController;
